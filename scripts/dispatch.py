a7b771591a5d4789a50033e7b2c17f7f5b5c49ea
patrick96 <p.ziegler96@gmail.com>
2022-05-26 23:50:36 +0200

    Write dispatch script

---
+++ scripts/dispatch.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python3
+"""
+Dispatches jobs on a cluster.
+
+This was written with the paths and assumptions of the cluster where the
+experiments were run.
+"""
+
+import os
+import sys
+import subprocess
+from pathlib import Path
+
+
+def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
+
+
+REPO_ROOT = Path(__file__).parent.parent.relative_to(Path.cwd())
+
+WRAPPER = REPO_ROOT / "scripts" / "wrapper.py"
+SEEDS = REPO_ROOT / "SMTLIB" / "QF_NIA-seeds"
+GEN = REPO_ROOT / "target" / "release" / "smtransform"
+
+HOME = Path.home()
+SOLVERS = HOME / 'smtransform' / 'solvers'
+
+
+def get_bsub(num: int, cmd: [str]) -> (str, [str]):
+    jobname = f'job{num}'
+    return jobname, [
+        'bsub',
+        '-W', '6:00',
+        '-J', jobname,
+        '-oo', f'out/out-{num}.log',
+        '-eo', f'out/err-{num}.log',
+        str(WRAPPER),
+        '--seeds', str(SEEDS),
+        '--out', f'out/{jobname}',
+        '--gen', str(GEN),
+        '--iterations', '20',
+        '--rounds', '10',
+        '--'
+    ] + cmd
+
+
+class Z3Runner:
+
+    @staticmethod
+    def get_exec(asan: bool) -> Path:
+        folder_name = f'z3-{"asan-" if asan else ""}8c95dff33'
+        return SOLVERS / folder_name / 'usr' / 'local' / 'bin' / 'z3'
+
+    @staticmethod
+    def get_cmd(asan: bool) -> (dict[str, str], [str]):
+        return ({}, [str(Z3Runner.get_exec(asan)), '/dev/stdin'])
+
+
+class Yices2Runner:
+
+    @staticmethod
+    def get_base(asan: bool) -> Path:
+        folder_name = f'yices2-{"asan-" if asan else ""}09f16210'
+        return SOLVERS / folder_name
+
+    @staticmethod
+    def get_exec(asan: bool) -> Path:
+        return Yices2Runner.get_base(asan) / 'usr' / 'local' / 'bin' / 'yices-smt2'
+
+    @staticmethod
+    def get_cmd(asan: bool) -> (dict[str, str], [str]):
+        ld_library_path = os.environ.get("LD_LIBRARY_PATH", "")
+        ld_library_path += f":{HOME}/smtransform/libpoly/build/pkg/usr/local/lib"
+        ld_library_path += f":{HOME}/smtransform/cudd/cudd/.libs"
+        return (
+            {"LD_LIBRARY_PATH": ld_library_path},
+            [str(Yices2Runner.get_exec(asan)), '/dev/stdin']
+        )
+
+
+class CVC5Runner:
+
+    @staticmethod
+    def get_base(asan: bool) -> Path:
+        folder_name = f'cvc5-{"asan-" if asan else ""}43f6eb50c'
+        return SOLVERS / folder_name
+
+    @staticmethod
+    def get_exec(asan: bool) -> Path:
+        return CVC5Runner.get_base(asan) / 'usr' / 'local' / 'bin' / 'cvc5'
+
+    @staticmethod
+    def get_cmd(asan: bool) -> (dict[str, str], [str]):
+        ld_library_path = os.environ.get("LD_LIBRARY_PATH", "")
+        ld_library_path += str(CVC5Runner.get_exec(asan).parent.parent / 'lib')
+        return (
+            {"LD_LIBRARY_PATH": ld_library_path},
+            [str(CVC5Runner.get_exec(asan)), '/dev/stdin']
+        )
+
+
+if __name__ == "__main__":
+    runners = [Z3Runner, Yices2Runner, CVC5Runner]
+
+    num = 0
+
+    for runner in runners:
+        for asan in [True, False]:
+
+            (env, solver_cmd) = runner.get_cmd(asan)
+            (name, cmd) = get_bsub(num, solver_cmd)
+
+            print(
+                f'{name}: {runner.__name__} {"ASAN" if asan else "RELEASE"}, env: {env}')
+
+            my_env = os.environ.copy()
+            my_env.update(env)
+
+            eprint(' '.join(cmd))
+            subprocess.run(cmd, check=True, env=my_env)
+
+            num += 1
